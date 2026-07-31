998c458086eb1ef4cc1c1938c7d11873d50c9a3b
KotonohaSora <sora.kotonoha5@gmail.com>
2026-08-01 06:18:18 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const today = new Date();
+const day = today.getDay(); 
+// 0: 日, 1: 月, 2: 火, 3: 水, 4: 木, 5: 金, 6: 土
+// 各曜日に対応するマスの位置（board-wrapper 内での位置）
+const positions = {
+    1: { top: 60, left: 9.5 },   // げつ
+    2: { top: 60, left: 23 },  // 
+    3: { top: 60, left: 36.5 },  // 
+    4: { top: 60, left: 56},  // 
+    5: { top: 60, left: 63.5 },  // 
+    6: { top: 60, left: 82.5},  // 
+    0: { top: 60, left: 96}   // 
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    const pin = document.getElementById('pin');
+
+    const today = new Date();
+    const day = today.getDay(); // 0〜6
+
+    const pos = positions[day];
+
+    if (pos && pin) {
+        // ピンの位置を設定（%で指定）
+        pin.style.top = pos.top + '%';
+        pin.style.left = pos.left + '%';
+    }
+});
