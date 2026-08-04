ebbc19207f3c99040c2d9fc35dac76e7b84f878c
KotonohaSora <sora.kotonoha5@gmail.com>
2026-08-04 10:29:04 +0900

    VS

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const day = today.getDay();
 // 0: 日, 1: 月, 2: 火, 3: 水, 4: 木, 5: 金, 6: 土
 // 各曜日に対応するマスの位置（board-wrapper 内での位置）
 const positions = {
-    1: { top: 60, left: 9.5 },   // げつ
-    2: { top: 60, left: 23 },  // 
-    3: { top: 60, left: 36.5 },  // 
-    4: { top: 60, left: 56},  // 
-    5: { top: 60, left: 63.5 },  // 
-    6: { top: 60, left: 82.5},  // 
-    0: { top: 60, left: 96}   // 
+    1: { top: 64, left: 9.5 },   // げつ
+    2: { top: 64, left: 23 },  // 
+    3: { top: 64, left: 36.5 },  // 
+    4: { top: 64, left: 56},  // 
+    5: { top: 64, left: 63.5 },  // 
+    6: { top: 64, left: 82.5},  // 
+    0: { top: 64, left: 96}   // 
 };
 
 window.addEventListener('DOMContentLoaded', () => {
